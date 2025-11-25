7eb205e86e57399ba6fb7db442bafe14c2172096
Luiza Michielin <luiza_reinhardt@estudante.sesisenai.org.br>
2025-11-25 20:08:37 -0300

    setima versao

---
+++ eletro8.js
@@ -0,0 +1,24 @@
+function calcularOhm() {
+    let V = parseFloat(document.getElementById("tensao").value);
+    let I = parseFloat(document.getElementById("corrente").value);
+    let R = parseFloat(document.getElementById("resistencia").value);
+    let resultado = document.getElementById("resultado");
+
+    // Cálculos
+    if (isNaN(V) && !isNaN(I) && !isNaN(R)) {
+        V = I * R;
+        resultado.innerHTML = `Tensão calculada: <strong>${V} V</strong>`;
+    } 
+    else if (isNaN(I) && !isNaN(V) && !isNaN(R)) {
+        I = V / R;
+        resultado.innerHTML = `Corrente calculada: <strong>${I} A</strong>`;
+    } 
+    else if (isNaN(R) && !isNaN(V) && !isNaN(I)) {
+        R = V / I;
+        resultado.innerHTML = `Resistência calculada: <strong>${R} Ω</strong>`;
+    }
+    else {
+        resultado.innerHTML = "Preencha exatamente dois campos!";
+    }
+}
+
